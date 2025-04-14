169e90c80d2ade279cb44c6b36d5e80f4e9f868c
shen lee <shenleekhalid@gmail.com>
2025-04-14 21:58:53 +0300

    added gettext_lazy for translation

--- orders/models.py
+++ orders/models.py
@@ -1,27 +1,33 @@
-from django.db import models
-from shop.models import Product
-from django.conf import settings
 from decimal import Decimal
 from django.core.validators import MinValueValidator, \
-    MaxValueValidator
+                                   MaxValueValidator
+from django.db import models
+from django.conf import settings
+from django.utils.translation import gettext_lazy as _
+from shop.models import Product
 from coupons.models import Coupon
 
 
-# Create your models here.
 class Order(models.Model):
-    first_name = models.CharField(max_length=200)
-    last_name = models.CharField(max_length=200)
-    email = models.EmailField()
-    address = models.CharField(max_length=200)
-    postal_code = models.CharField(max_length=20)
-    city = models.CharField(max_length=100)
+    first_name = models.CharField(_('first name'),
+                                  max_length=50)
+    last_name = models.CharField(_('last name'),
+                                 max_length=50)
+    email = models.EmailField(_('e-mail'))
+    address = models.CharField(_('address'),
+                               max_length=250)
+    postal_code = models.CharField(_('postal code'),
+                                   max_length=20)
+    city = models.CharField(_('city'),
+                            max_length=100)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False)
     stripe_id = models.CharField(max_length=250, blank=True)
     coupon = models.ForeignKey(Coupon,
-                               related_name='order',
-                               null=True, blank=True,
+                               related_name='orders',
+                               null=True,
+                               blank=True,
                                on_delete=models.SET_NULL)
     discount = models.IntegerField(default=0,
                                    validators=[MinValueValidator(0),
@@ -30,14 +36,24 @@ class Order(models.Model):
     class Meta:
         ordering = ['-created']
         indexes = [
-            models.Index(fields=['created']),
+            models.Index(fields=['-created']),
         ]
 
     def __str__(self):
         return f'Order {self.id}'
 
-    def get_total_cost(self):
+    def get_total_cost_before_discount(self):
         return sum(item.get_cost() for item in self.items.all())
+
+    def get_discount(self):
+        total_cost = self.get_total_cost_before_discount()
+        if self.discount:
+            return total_cost * (self.discount / Decimal(100))
+        return Decimal(0)
+
+    def get_total_cost(self):
+        total_cost = self.get_total_cost_before_discount()
+        return total_cost - self.get_discount()
 
     def get_stripe_url(self):
         if not self.stripe_id:
@@ -59,7 +75,8 @@ class OrderItem(models.Model):
     product = models.ForeignKey(Product,
                                 related_name='order_items',
                                 on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10,
+                                decimal_places=2)
     quantity = models.PositiveIntegerField(default=1)
 
     def __str__(self):
@@ -67,16 +84,3 @@ class OrderItem(models.Model):
 
     def get_cost(self):
         return self.price * self.quantity
-
-    def get_total_cost_before_discount(self):
-        return sum(item.get_cost() for item in self.item.all())
-
-    def get_discount(self):
-        total_cost = self.get_total_cost_before_discount()
-        if self.discount:
-            return total_cost * (self.discount / (100))
-        return Decimal(0)
-
-    def get_total_cost(self):
-        total_cost = self.get_total_cost_before_discount()
-        return total_cost - self.get_discount()
