d9320544b1ae25f136f7e1931c3bee9156c3dab5
shen lee <shenleekhalid@gmail.com>
2025-05-29 14:41:11 +0300

    added sitemaps

--- myshop/urls.py
+++ myshop/urls.py
@@ -17,10 +17,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
+from shop.sitemaps import ProductSitemap
 
+
+sitemaps = {
+    'products': ProductSitemap,
+}
 
 urlpatterns = [
     path('secret-portal-49u2a2j/', admin.site.urls),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
+         name='django.contrib.sitemaps.views.sitemap'),
     path('cart/', include('cart.urls', namespace='cart')),
     path('orders/', include('orders.urls', namespace='orders')),
     path('payment/', include('payment.urls', namespace='payment')),
---
+++ shop/sitemaps.py
@@ -0,0 +1,13 @@
+from django.contrib.sitemaps import Sitemap
+from .models import Product
+
+
+class ProductSitemap(Sitemap):
+    changefreq = "weekly"
+    priority = 0.9
+
+    def items(self):
+        return Product.objects.filter(available=True)
+
+    def lastmod(self, obj):
+        return obj.updated
